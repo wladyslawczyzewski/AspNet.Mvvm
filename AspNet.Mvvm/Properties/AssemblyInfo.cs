d16d49b52acd9cb45b60a6dfad7ea0915d754d2a
Vladyslav Chyzhevskyi <coirius@coirius.com>
2014-03-16 15:44:02 +0100

    init

---
+++ AspNet.Mvvm/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AspNet.Mvvm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNet.Mvvm")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5fc171fa-15fb-4c2a-8564-e6b218838888")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
